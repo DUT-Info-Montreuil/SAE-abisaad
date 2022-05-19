4d5b8b9cc0c79bd9e9786cf642f941d1b31bb7a1
mpouchin <mathys.pouchin21@gmail.com>
2022-05-19 16:35:34 +0200

    avec la librairi Json

--- src/main/java/fr/sae/aquilius/model/Personnage.java
+++ src/main/java/fr/sae/aquilius/model/Personnage.java
@@ -7,13 +7,11 @@ public class Personnage {
 
     private IntegerProperty y;
     private IntegerProperty x;
-    private boolean keypressed;
-    private boolean keycode;
 
     public Personnage (int x, int y) {
         super();
-        this.x = new SimpleIntegerProperty();
-        this.y = new SimpleIntegerProperty();
+        this.x = new SimpleIntegerProperty(x);
+        this.y = new SimpleIntegerProperty(y);
         int vitesse = 32;
     }
 
@@ -30,37 +28,16 @@ public class Personnage {
         this.y = y;
     }
 
-/*    public void deplacPers(){
-        if(keypressed == true){
-            if (Z) {
-                this.getY().setValue(this.getY().getValue() + 32);
-            } else if(keycode == DOWN){
-                this.getY().setValue(this.getY().getValue()-32);
-            }
-            else if(keycode == RIGHT){
-                this.x.set(this.getX().getValue()+32);
-            }
-            else if(keycode == LEFT){
-                this.x.set(this.getX().getValue()-32);
-            }
-        }
-
-
-    }*/
-
-    public void DeplacementHeroDroite () {
+    public void DeplacementHeroDroite() {
         this.x.set(this.getX().getValue()+32);
     }
-
-    public void DeplacementHeroGauche () {
+    public void DeplacementHeroGauche() {
         this.x.set(this.getX().getValue()-32);
     }
-
-    public void DeplacementHeroHaut () {
+    public void DeplacementHeroHaut() {
         this.getY().setValue(this.getY().getValue()+32);
     }
-
-    public void DeplacementHeroBas () {
+    public void DeplacementHeroBas() {
         this.getY().setValue(this.getY().getValue()-32);
     }
 }
